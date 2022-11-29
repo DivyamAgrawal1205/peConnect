086ac84f9d6ad64e53103866da21a3466cebb8af
Nitin Singla <78803509+singlanitin661@users.noreply.github.com>
2022-11-29 12:21:51 +0530

    Update NotificationSchema.js

--- schemas/NotificationSchema.js
+++ schemas/NotificationSchema.js
@@ -1,6 +1,6 @@
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
 const NotificationSchema = new Schema({
     userTo: { type: Schema.Types.ObjectId, ref: 'User' },
     userFrom: { type: Schema.Types.ObjectId, ref: 'User' },
