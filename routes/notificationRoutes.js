865fdcfd55c6fcabe74152dbcdb094b1b60ab6e0
AryanJindal <94097852+AryanJindal@users.noreply.github.com>
2022-11-29 12:32:42 +0530

    Update notificationRoutes.js

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
+
 const app = express();
+
 const router = express.Router();
+
 const bodyParser = require("body-parser")
 const bcrypt = require("bcrypt");
 const mongoose = require('mongoose');
