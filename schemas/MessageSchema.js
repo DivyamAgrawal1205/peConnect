8c8801100f5367a6e2e78c8ba09029db01c076db
AryanJindal <94097852+AryanJindal@users.noreply.github.com>
2022-11-29 11:20:32 +0530

    Update MessageSchema.js

--- schemas/MessageSchema.js
+++ schemas/MessageSchema.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
+    
     sender: { type: Schema.Types.ObjectId, ref: 'User' },
     content: { type: String, trim: true },
     chat: { type: Schema.Types.ObjectId, ref: 'Chat' },
