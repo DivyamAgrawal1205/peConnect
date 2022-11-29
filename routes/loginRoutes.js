bca603dac829adc69f12ad95ddd6910d57d1b9a2
AryanJindal <94097852+AryanJindal@users.noreply.github.com>
2022-11-29 12:30:09 +0530

    Update loginRoutes.js

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -4,6 +4,7 @@ const app = express();
 const router = express.Router();
 
 const bodyParser = require("body-parser")
+
 const bcrypt = require("bcrypt");
 const User = require('../schemas/UserSchema');
 
