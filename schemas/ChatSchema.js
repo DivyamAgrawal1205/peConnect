c0b4f6d9ba04ae3cfe78979ad492a6531fade053
AryanJindal <94097852+AryanJindal@users.noreply.github.com>
2022-11-29 11:19:48 +0530

    Setting up chat schemas

--- schemas/ChatSchema.js
+++ schemas/ChatSchema.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+
 const chatSchema = new Schema({
     chatName: { type: String, trim: true },
     isGroupChat: { type: Boolean, default: false },
